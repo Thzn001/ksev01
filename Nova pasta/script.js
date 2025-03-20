76582cd97994f85c8eaeacc074d60f58741e16f6
Thzn001 <thguimaraes1906@gmail.com>
2025-03-20 00:57:55 -0300

    Add files via upload

---
+++ Nova pasta/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const botao = document.getElementById("mais-sobre");
+
+    botao.addEventListener("click", function () {
+        window.location.href = "https://www.instagram.com/kauanysev?igsh=dHJqc2FucjAxYmtm";
+    });
+});
